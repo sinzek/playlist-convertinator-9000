3f0ea7edf1c79aca74242b71a58d8998c1ca4cc6
Chase Brock <chase.p.brock@gmail.com>
2024-10-28 03:43:47 -0500

    Added routing with react-router-dom, template pages, useState for active page buttons in navbar, and a 404 page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
+  BrowserRouter
 } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Home from "./pages/Home.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
 import Convert from "./pages/Convert.jsx";
+import Account from "./pages/Account.jsx";
+import Login from "./pages/Login.jsx";
 
 const router = createBrowserRouter([
   {
@@ -18,12 +21,22 @@ const router = createBrowserRouter([
   {
     path: "convert",
     element: <Convert />,
-  }
+  },
+  {
+    path: "account",
+    element: <Account />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<Navbar />
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
     <RouterProvider router={router} />
 	</StrictMode>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
 export default function Home() {
     return(
         <>
-          home page
+            <div className="flex w-full flex-col lg:flex-row">
+                <div className="card bg-base-200 lg:ml-5 rounded-box grid flex-grow w-1/2 p-5">
+                    <div>
+                        <h1 className="text-5xl font-bold mb-2 text-primary">Transfer your playlists</h1>
+                        <h2 className="text-2xl font-italic mb-10">Chase's reliable Apple Music to YouTube Music playlist converter</h2>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+                <div className="divider lg:divider-horizontal" />
+                <div className="card bg-base-200 lg:mr-5 rounded-box grid h-32 flex-grow place-items-center p-5">
+                    content
+                </div>
+            </div>
         </>
     )
 }
